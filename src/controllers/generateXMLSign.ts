7278466fab400b4bf1c4eba2821689f0d671c023
unknown <jrsantur06@gmail.com>
2023-10-21 21:49:55 -0500

    Update xml signed

--- src/controllers/generateXMLSign.ts
+++ src/controllers/generateXMLSign.ts
@@ -10,15 +10,17 @@ const generateXMLSign = (req: Request, res: Response, next: NextFunction) => {
 
     var xml :string = req.body.xml;   
 
-    xmlgen.signXML(xml, '../7155053_identity.p12' , '123456').then(xmlSigned => {
+    xmlgen.signXML(xml, '../7155053_identity.p12' , 'Sandra09').then(xmlSigned => {
         console.log("XML firmado", xmlSigned)
 
         return res.status(200).json({
             message: xmlSigned
         });  
     }).catch(error => {
-        res.json(error);
         console.log(error);
+        return res.status(500).json({
+            message: error
+        });
     });     
 
 }
